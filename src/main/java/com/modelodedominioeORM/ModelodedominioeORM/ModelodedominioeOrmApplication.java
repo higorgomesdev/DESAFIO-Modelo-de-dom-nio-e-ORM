acc18df45c91d46fbf72ffb6486c53f5b241a270
Higor Gomes <higorddtank123@gmail.com>
2026-04-16 15:36:45 -0300

    Iniciando o desafio

---
+++ src/main/java/com/modelodedominioeORM/ModelodedominioeORM/ModelodedominioeOrmApplication.java
@@ -0,0 +1,13 @@
+package com.modelodedominioeORM.ModelodedominioeORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelodedominioeOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelodedominioeOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modelodedominioeORM/ModelodedominioeORM/ModelodedominioeOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelodedominioeORM.ModelodedominioeORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelodedominioeOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
