ea629ac6e7b2ea92e72c74e320eefe60e6c2f0f6
Higor Gomes <higorddtank123@gmail.com>
2026-04-16 16:08:52 -0300

    Entidades categoria, atividade ( muitos para um )

---
+++ src/main/java/com/modelodedominioeORM/ModelodedominioeORM/entities/Atividade.java
@@ -0,0 +1,79 @@
+package com.modelodedominioeORM.ModelodedominioeORM.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String descricao;
+	private Double price;
+	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	private Categoria categoria;
+	
+	public Atividade() {
+	}
+
+	public Atividade(Integer id, String name, String descricao, Double price, Categoria categoria) {
+		this.id = id;
+		this.name = name;
+		this.descricao = descricao;
+		this.price = price;
+		this.categoria = categoria;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+}
